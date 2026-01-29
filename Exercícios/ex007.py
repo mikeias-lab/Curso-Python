a9faf1a756a865ca351dd4c17dbe75df387d4ab0
maic <usuario@dominio.com>
2026-01-28 21:16:40 -0300

    alterações do dia

---
+++ Exercícios/ex007.py
@@ -0,0 +1,5 @@
+n1 = float(input('Nota do primeiro aluno: '))
+n2 = float(input('Nota do segundo aluno: '))
+n3 = float(input('Nota do terceiro aluno: '))
+media = (n1+n2+n3) / 3
+print('A média da sala foi {}'.format(media))
