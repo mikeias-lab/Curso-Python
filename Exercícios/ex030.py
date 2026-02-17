2dc9a24b91dfb4be7ec10dc56daec3e1527bb72b
maic <usuario@dominio.com>
2026-02-17 20:09:14 -0300

    Alterações do dia 17/02

---
+++ Exercícios/ex030.py
@@ -0,0 +1,10 @@
+'''Crie um programa que leia um número inteiro
+e mostre na tela se ele é PAR ou ÍMPAR.'''
+
+#minha solução
+
+n = int(input('Digite um número qualquer: '))
+if n % 2 == 0:
+    print('É PAR')
+else:
+    print('É ÍMPAR')
