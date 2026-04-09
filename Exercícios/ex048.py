ab2593085422b08251781f3ab1bd9d11dab8480c
maic <usuario@dominio.com>
2026-04-08 21:11:54 -0300

    Alterações do dia 08/04

--- Exercícios/ex048.py
+++ Exercícios/ex048.py
@@ -4,7 +4,7 @@ múltiplos de três e que se encontram no intervalo de 1 até 500.'''
 #minha solução
 
 soma = 0
-for c in range(1, 50):
+for c in range(1, 500):
     if c % 2 != 0 and c % 3 == 0:
         soma += c #precisei buscar na internet essa parte da soma
 print(soma)
--- Exercícios/ex052.py
+++ Exercícios/ex052.py
@@ -1,1 +1,18 @@
 '''programa que leia um número inteiro e diga se ele é ou não um número primo.'''
+
+#solução do professor
+
+num = int(input('Digite um número: '))
+total = 0 #variante pra somar quantas vezes foram divididos
+for c in range (1, num + 1):
+    if num % c == 0:
+        print('\033[33m', end='')
+        total += 1
+    else:
+        print('\033[31m', end='')
+    print('{} '.format(c), end='')
+print('\n\033[mOnúmero {} foi divisível {} vezes'.format(num, total))
+if total == 2:
+    print('Então ele É UM NÚMERO PRIMO')
+else:
+    print('Então ele NÃO É UM NÚMERO PRIMO')
